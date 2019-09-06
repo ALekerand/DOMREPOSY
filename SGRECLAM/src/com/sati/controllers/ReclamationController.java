064e5b4358def4d7cbc5196d742c68d11a68222b
MACHELI <MACHELI@DESKTOP-5H78ASU>
2019-09-06 13:08:36 +0000

    premiere commite

---
+++ SGRECLAM/src/com/sati/controllers/ReclamationController.java
@@ -0,0 +1,190 @@
+/**
+ * 
+ */
+package com.sati.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.component.commandbutton.CommandButton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sati.model.Ecue;
+import com.sati.model.Evaluation;
+import com.sati.model.MotifeReclamation;
+import com.sati.model.Reclamation;
+import com.sati.model.Sexe;
+import com.sati.model.TypeEvaluation;
+import com.sati.service.Iservice;
+
+/**
+ * @author MACHELI
+ *
+ */
+@Component
+public class ReclamationController {
+	
+	@Autowired
+	Iservice service;
+	
+	private Reclamation reclamation = new Reclamation();
+	private List<Reclamation> listReclamation = new ArrayList<Reclamation>();
+	private Reclamation selectedReclamation = new Reclamation();
+	private List<Evaluation> listEvaluation = new ArrayList<Evaluation>();
+	private Evaluation selectedEvaluation = new Evaluation();
+	private List<MotifeReclamation> listMotifeReclamation = new ArrayList<MotifeReclamation>();
+	private MotifeReclamation choisirMotifeReclamation = new MotifeReclamation(); 
+	private int codeMotifReclam;
+
+
+
+	private CommandButton btnEnregistrer = new CommandButton();
+	private CommandButton btnAnnuler = new CommandButton();
+	private CommandButton btnModifier = new CommandButton();
+	
+	@PostConstruct
+	public void initialiser() {
+		btnModifier.setDisabled(true);
+	}
+	
+	
+	public void enregistrer() {
+		choisirMotifeReclamation = (MotifeReclamation) service.getObjectById(codeMotifReclam, "MotifeReclamation");	
+		reclamation.setEvaluation(selectedEvaluation);
+		
+		service.addObject(reclamation);
+		info("Eneregistrement éffectué avec succès!");
+		annuler();
+		
+	}
+	
+	public void selectionnerLigne() {
+		reclamation = selectedReclamation;
+		 btnEnregistrer.setDisabled(true);
+		 btnModifier.setDisabled(false);
+	}
+	
+	
+	
+	public void info(String monMessage) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", monMessage));
+    }
+
+ public void error() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Contact admin."));
+    }
+ 
+ public void annuler() {
+	 reclamation.setNumeroReclam(0);
+	 reclamation.setDateRetourReclam(null);
+	btnModifier.setDisabled(true);
+	btnEnregistrer.setDisabled(false);
+ }
+
+ public void modifier() {
+	 service.updateObject(reclamation);
+	 annuler();
+	 info("Modification effectué avec succés!");
+ }
+	
+
+	public Reclamation getReclamation() {
+		return reclamation;
+	}
+
+	public void setReclamation(Reclamation reclamation) {
+		this.reclamation = reclamation;
+	}
+
+	public List<Reclamation> getListReclamation() {
+		listReclamation = service.getObjects("Reclamation");
+		return listReclamation;
+	}
+
+	public void setListReclamation(List<Reclamation> listReclamation) {
+		this.listReclamation = listReclamation;
+	}
+
+	public Reclamation getSelectedReclamation() {
+		return selectedReclamation;
+	}
+
+	public void setSelectedReclamation(Reclamation selectedReclamation) {
+		this.selectedReclamation = selectedReclamation;
+	}
+
+	public CommandButton getBtnEnregistrer() {
+		return btnEnregistrer;
+	}
+
+	public void setBtnEnregistrer(CommandButton btnEnregistrer) {
+		this.btnEnregistrer = btnEnregistrer;
+	}
+
+	public CommandButton getBtnAnnuler() {
+		return btnAnnuler;
+	}
+
+	public void setBtnAnnuler(CommandButton btnAnnuler) {
+		this.btnAnnuler = btnAnnuler;
+	}
+
+	public CommandButton getBtnModifier() {
+		return btnModifier;
+	}
+
+	public void setBtnModifier(CommandButton btnModifier) {
+		this.btnModifier = btnModifier;
+	}
+
+	public List<Evaluation> getListEvaluation() {
+		listEvaluation = service.getObjects("Evaluation");
+		return listEvaluation;
+	}
+
+	public void setListEvaluation(List<Evaluation> listEvaluation) {
+		this.listEvaluation = listEvaluation;
+	}
+
+	public Evaluation getSelectedEvaluation() {
+		return selectedEvaluation;
+	}
+
+	public void setSelectedEvaluation(Evaluation selectedEvaluation) {
+		this.selectedEvaluation = selectedEvaluation;
+	}
+
+
+	public List<MotifeReclamation> getListMotifeReclamation() {
+		listMotifeReclamation = service.getObjects("MotifeReclamation");
+		return listMotifeReclamation;
+	}
+
+
+	public void setListMotifeReclamation(List<MotifeReclamation> listMotifeReclamation) {
+		this.listMotifeReclamation = listMotifeReclamation;
+	}
+
+
+	public MotifeReclamation getChoisirMotifeReclamation() {
+		return choisirMotifeReclamation;
+	}
+
+
+	public void setChoisirMotifeReclamation(MotifeReclamation choisirMotifeReclamation) {
+		this.choisirMotifeReclamation = choisirMotifeReclamation;
+	}
+	public int getCodeMotifReclam() {
+		return codeMotifReclam;
+	}
+
+
+	public void setCodeMotifReclam(int codeMotifReclam) {
+		this.codeMotifReclam = codeMotifReclam;
+	}
+}
