6e2a11446afced997575969233c356edd78ac0f7
MACHELI <MACHELI@DESKTOP-5H78ASU>
2019-09-13 17:50:48 +0000

    le commit du jour

---
+++ SGRECLAM/src/com/sati/controllers/ConsultationReclamController.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.sati.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.sati.model.Reclamation;
+import com.sati.service.Iservice;
+
+/**
+ * @author MACHELI
+ *
+ */
+@Component
+public class ConsultationReclamController {
+@Autowired
+Iservice service;
+
+private List<Reclamation> listReclamation = new ArrayList<Reclamation>();
+
+public List<Reclamation> getListReclamation() {
+	
+	listReclamation = service.getObjects("Reclamation");
+	return listReclamation;
+}
+
+public void setListReclamation(List<Reclamation> listReclamation) {
+	this.listReclamation = listReclamation;
+}
+	
+}
