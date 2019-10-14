06ca71be66b03de073f5c7fac3556f0ca166b39e
HP <HP@THESEVOSKI2>
2019-10-14 18:53:27 +0000

    Exemple requete 2

---
+++ SGRECLAM/src/com/sati/requettes/RequeteEvaluation.java
@@ -0,0 +1,30 @@
+package com.sati.requettes;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sati.model.Evaluation;
+
+@Transactional
+@Component
+public class RequeteEvaluation {
+	
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	
+	
+	public List<Evaluation> recupEvaluationBySemestre(String codeSemestre){
+		
+	String query = "SELECT * FROM evaluation WHERE evaluation.CODE_SEMESTRE = '"+codeSemestre+"'";
+		
+		List liste = sessionFactory.getCurrentSession().createSQLQuery(query).addEntity(Evaluation.class).list();
+		return liste;
+		
+	}
+	
+}
